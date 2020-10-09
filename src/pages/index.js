54b22b2ba521e5d5c5267eb8f9015066cea4864c
Muh. Yasin Ardhi Sawal <muh.yasin.ardhi.sawal@gmail.com>
2020-10-09 08:53:33 +0700

    create contents builder at strapi for projects and blogs, create its contents, take its query, rendering it at front end.

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,13 +6,63 @@ import Services from "../components/Services"
 import Jobs from "../components/Jobs"
 import Projects from "../components/Projects"
 import Blogs from "../components/Blogs"
-export default () => {
+
+export default ({ data }) => {
+  const {
+    allStrapiProjects: { nodes: projects },
+    allStrapiBlogs: { nodes: blogs },
+  } = data
+
   return (
     <Layout>
       <Hero />
       <Services />
       <Jobs />
+      <Projects projects={projects} title="featured projects" showLink />
+      <Blogs blogs={blogs} title="latest article" showLink />
     </Layout>
   )
 }
-// ...GatsbyImageSharpFluid
+
+export const query = graphql`
+  {
+    allStrapiProjects(filter: { featured: { eq: true } }) {
+      nodes {
+        github
+        id
+        title
+        url
+        description
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        stack {
+          id
+          title
+        }
+      }
+    }
+    allStrapiBlogs(limit: 3, sort: { fields: date, order: DESC }) {
+      nodes {
+        category
+        slug
+        content
+        desc
+        date(formatString: "MMMM Do, YYYY")
+        id
+        title
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
